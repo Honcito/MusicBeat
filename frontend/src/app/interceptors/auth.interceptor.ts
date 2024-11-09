9b88ddf7de6c55897003b08880c80b04706f7629
Victor Hong <vhoncito@gmail.com>
2024-11-09 16:38:28 +0000

    fixed login, profile auth.service

---
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,25 @@
+
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Obtén el token de sessionStorage
+    const token = sessionStorage.getItem('token');
+
+    // Si el token existe, agregamos la cabecera Authorization
+    if (token) {
+      const cloned = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return next.handle(cloned);
+    }
+
+    // Si no hay token, la solicitud se maneja sin modificar
+    return next.handle(req);
+  }
+}
